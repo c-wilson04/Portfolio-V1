d976a2ba26a18ca389864458d6bb39bdd29be064
c-wilson04 <141252422+c-wilson04@users.noreply.github.com>
2023-08-07 09:30:46 -0400

    Update main.js
    changed file direction to dist folder

--- main.js
+++ main.js
@@ -23,7 +23,7 @@ scene.background = new THREE.Color('Black')
 scene.add(camera)
 
 
-loader.load( './Car/scene.gltf', function ( gltf ) {
+loader.load( './dist/Car/scene.gltf', function ( gltf ) {
 	scene.add( gltf.scene );
     secondOBJ= gltf.scene.children[0];
     secondOBJ.scale.set(5,5,5);
@@ -113,7 +113,7 @@ scene2.add(camera2)
 
 
 
-loader2.load( './WireFrame/WireFrameFace_Omar.gltf', function ( gltf ) {
+loader2.load( './dist//WireFrame/WireFrameFace_Omar.gltf', function ( gltf ) {
 	scene2.add( gltf.scene );
     canvas2OBJ= gltf.scene.children[0];
     canvas2OBJ.scale.set(2,2,2);
