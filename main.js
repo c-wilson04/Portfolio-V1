3463ecee986816049bbcf0a50862329d4535bfd7
c-wilson04 <141252422+c-wilson04@users.noreply.github.com>
2023-08-14 12:09:45 -0400

    Update main.js

--- main.js
+++ main.js
@@ -44,10 +44,10 @@ const geometry = new THREE.TorusGeometry(15, 3, 16, 100)
 const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
 
-const pointLight = new THREE.PointLight(0xffffff,2)
+const pointLight = new THREE.PointLight(0xffffff,0)
 pointLight.position.set(15,5,5)
 
-const ambientLight= new THREE.AmbientLight(0xffffff,1)
+const ambientLight= new THREE.AmbientLight(0xffffff,0)
 ambientLight.position.set(15,5,5)
 scene.add(pointLight,ambientLight)
 
