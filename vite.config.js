72d5ebbb952e8c07f38d2736588926f7f5cb000f
Charles Wilson <charleswilsonk10@gmail.com>
2023-11-09 04:06:28 -0500

    update

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { resolve } from 'path'
 
 export default defineConfig({
   assetsInclude: ['**/*.gltf'],
+  base: '/c-wilson04.github.io/Portfolio-V1',
     build: {
       rollupOptions: {
         input: {
